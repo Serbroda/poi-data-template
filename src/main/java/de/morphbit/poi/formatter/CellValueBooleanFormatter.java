132a637a44184cc4751ef8cd460f95bf6c1fa24f
Danny Rottstegge <d.rottstegge@evosec.de>
2018-07-12 16:09:41 +0200

    Add possibility to pass true and false pattern

--- src/main/java/de/morphbit/poi/formatter/CellValueBooleanFormatter.java
+++ src/main/java/de/morphbit/poi/formatter/CellValueBooleanFormatter.java
@@ -4,37 +4,65 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 
 public class CellValueBooleanFormatter extends CellValueFormatter<Boolean> {
-	
+
 	public static final String DEFAULT_FALSE_REGEX = "^(0)|(false)|(nein)|(falsch)|(no)|(n)$";
 	public static final String DEFAULT_TRUE_REGEX = "^(1)|(true)|(ja)|(richtig)|(yes)|(y)|(j)|(wahr)$";
-	
+
+	private String falsePattern;
+	private String truePattern;
+
+	public CellValueBooleanFormatter() {
+		this(DEFAULT_FALSE_REGEX, DEFAULT_TRUE_REGEX);
+	}
+
+	public CellValueBooleanFormatter(String falsePattern, String truePattern) {
+		this.falsePattern = falsePattern;
+		this.truePattern = truePattern;
+	}
+
+	public String getFalsePattern() {
+		return falsePattern;
+	}
+
+	public void setFalsePattern(String falsePattern) {
+		this.falsePattern = falsePattern;
+	}
+
+	public String getTruePattern() {
+		return truePattern;
+	}
+
+	public void setTruePattern(String truePattern) {
+		this.truePattern = truePattern;
+	}
+
 	@Override
 	public Boolean format(Cell cell) {
-		if(cell == null) {
+		if (cell == null) {
 			return null;
 		}
 		CellType type = getEffectiveCellType(cell);
-		if(type == CellType.BOOLEAN) {
+		if (type == CellType.BOOLEAN) {
 			return cell.getBooleanCellValue();
-		 } else if (type == CellType.NUMERIC) {
-			 if(cell.getNumericCellValue() == 0) {
-				 return false;
-			 } else if (cell.getNumericCellValue() == 1) {
-				 return true;
-			 } else {
-				 return null;
-			 }
-		 } else if (type == CellType.STRING) {
-			 String value = cell.getStringCellValue().toLowerCase();
-			 if(value.matches(DEFAULT_FALSE_REGEX)) {
-				 return false;
-			 } else if (value.matches(DEFAULT_TRUE_REGEX)) {
-				 return true;
-			 } else {
-				 return null;
-			 }
-		 }
-		
+		} else if (type == CellType.NUMERIC) {
+			if (cell.getNumericCellValue() == 0) {
+				return false;
+			} else if (cell.getNumericCellValue() == 1) {
+				return true;
+			} else {
+				return null;
+			}
+		} else if (type == CellType.STRING) {
+			String value = cell.getStringCellValue().toLowerCase();
+			if (value.matches(getFalsePattern())) {
+				return false;
+			} else if (value.matches(getTruePattern())) {
+				return true;
+			} else {
+				return null;
+			}
+		}
+
 		return null;
 	}
 
